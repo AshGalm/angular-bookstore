0f481b53a7218916d42beb897fb6a2b7388aaa2f
aisha <outsiderash@gmail.com>
2024-03-01 13:41:50 +0200

    Done

--- src/app/Services/requests.service.ts
+++ src/app/Services/requests.service.ts
@@ -1,30 +1,18 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { map, filter } from 'rxjs/operators';
-import { Book } from '../home/book-details/book-details.model';
+// import { Injectable } from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
+// import { Observable } from 'rxjs';
+// import { map, filter } from 'rxjs/operators';
+// import { Book } from '../home/book-details/book-details.model';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class DataService {
-  public api ='assets/Database/localData.json'
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class DataService {
 
-  constructor(private http:HttpClient) { }
 
-  getJsonData(){
-    return this.http.get('assets/Database/localData.json');
-  }
+//   constructor(private http:HttpClient, dataService:DataService) { }
 
-  getBook(id:number) : Observable<Book>{
-    return this.http.get<Book[]>(this.api).pipe(
-      map((books: Book[]) => books.find((book: Book) => book.id === id)),
-      filter((book: Book | undefined): book is Book => book !== undefined)
-    );
-  }
 
-  addBook(book:Book): Observable<Book>{
-    return this.http.post<Book>(this.api, book);
-  }
 
-}
+
+// }
--- src/app/config/api.ts
+++ src/app/config/api.ts
@@ -1,9 +1,9 @@
-import { Injectable } from "@angular/core";
+// import { Injectable } from "@angular/core";
 
-@Injectable({
-  providedIn: "root"
-})
-export class SomeService {
-  // Api URL
-  public api = "../../assets/Database/localData.json";
-}
+// @Injectable({
+//   providedIn: "root"
+// })
+// export class SomeService {
+//   // Api URL
+//   public api = "../../assets/Database/localData.json";
+// }
